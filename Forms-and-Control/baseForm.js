f9096bc54890736a94f1ee8228f6bc617bd9c53b
Émile Nestor <76712426+emilenestor@users.noreply.github.com>
2021-04-28 11:24:49 +0200

    Add files via upload

---
+++ Forms-and-Control/baseForm.js
@@ -0,0 +1,14 @@
+/*
+const myForm = document.getElementById("myForm");
+
+myForm.addEventListener("submit", (e) => {
+    myForm.
+        e.preventDefault();
+
+});
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+*/
