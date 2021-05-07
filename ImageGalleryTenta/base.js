051a6ebcf9aeb38acec03d27fd2fb207bbb671b3
nejstore <emilnestor@hotmail.com>
2021-05-07 17:38:52 +0200

    Updated first page and css

--- ImageGalleryTenta/base.js
+++ ImageGalleryTenta/base.js
@@ -1,6 +1,7 @@
 const apiKey = "ed033607eb107d8e7d8c5f174bd2209d";
 const searchBar = document.querySelector(".searchBar");
 const searchBtn = document.querySelector(".searchBtn");
+const searchResult = document.querySelector(".searchResult");
 
 searchBtn.addEventListener("click", async function () {
     const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchBar.value}&per_page=20&sort=date-taken-asc&format=json&nojsoncallback=1`)
