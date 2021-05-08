02e95ec9e6d52c916af0bbf1f2898ab61e07ec07
nejstore <emilnestor@hotmail.com>
2021-05-08 18:42:40 +0200

    Added images on search.

--- ImageGalleryTenta/base.js
+++ ImageGalleryTenta/base.js
@@ -3,10 +3,26 @@ const searchBar = document.querySelector(".searchBar");
 const searchBtn = document.querySelector(".searchBtn");
 const searchResult = document.querySelector(".searchResult");
 
+//Image URL
+//https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
+
+
 searchBtn.addEventListener("click", async function () {
-    const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchBar.value}&per_page=20&sort=date-taken-asc&format=json&nojsoncallback=1`)
+    const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchTxt.value}&per_page=20&sort=relevance&format=json&nojsoncallback=1`)
     const data = await response.json()
     console.log(data);
+
+    data.photos.photo.forEach(photo => {
+
+        const serverId = photo.server;
+        const id = photo.id;
+        const secret = photo.secret;
+        const size = "q";
+        const thumbnail = document.createElement("img");
+        thumbnail.src = `https://live.staticflickr.com/${serverId}/${id}_${secret}_${size}.jpg`
+        searchResult.appendChild(thumbnail)
+        
+    });
 })
 
 searchBar.addEventListener("keyup", function (event) {
