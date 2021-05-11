dd898613ebc9c91315f4b6706aee6b143d48ea9d
nejstore <emilnestor@hotmail.com>
2021-05-11 23:56:56 +0200

    Added IntersectionObserver
    Loads more images after the user scrolls to the bottom of the page.

--- ImageGalleryTenta/base.js
+++ ImageGalleryTenta/base.js
@@ -3,9 +3,20 @@ const searchBar = document.querySelector(".searchBar");
 const searchTxt = document.querySelector(".searchTxt");
 const searchBtn = document.querySelector(".searchBtn");
 const searchResult = document.querySelector(".searchResult");
+const scrollInstruction = document.querySelector(".scrollInstruction");
+const footer = document.querySelector("footer");
 const lightbox = document.querySelector(".lightbox");
 const thumbnailSize = "q";
 const largeImgSize = "b";
+let page = 1;
+let imgsPerPage = 20;
+
+let options = {
+    root: null,
+    rootMargins: "0px",
+    threshold: 0.5
+};
+const observer = new IntersectionObserver(handleIntersect, options);
 
 //Image URL
 //https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
@@ -17,6 +28,7 @@ searchBtn.addEventListener("click", async function () {
     //console.log(data);
 
     searchResult.innerHTML = "";
+    page = 1;
 
     data.photos.photo.forEach(photo => {
         const serverId = photo.server;
@@ -29,16 +41,58 @@ searchBtn.addEventListener("click", async function () {
 
     //Makes the images clickable for a larger version
     const searchResultImgs = document.querySelectorAll(".searchResult img");
-    searchResultImgs.forEach(img => {
-        img.addEventListener("click", function () {
-            //console.log(img.src);
+    for (let index = imgsPerPage * (page - 1); index < searchResultImgs.length; index++) {
+
+        searchResultImgs[index].addEventListener("click", function () {
             lightbox.classList.add("active");
             const largeImg = document.createElement("img");
-            largeImg.src = img.src.replace(`_${thumbnailSize}.jpg`, `_${largeImgSize}.jpg`);
+            largeImg.src = searchResultImgs[index].src.replace(`_${thumbnailSize}.jpg`, `_${largeImgSize}.jpg`);
             lightbox.appendChild(largeImg);
         });
-    });
+    }
+
+    scrollInstruction.style.display = "flex";
+
+    footer.style.display = "flex";
+
+    observer.observe(footer);
 });
+
+function handleIntersect(entries) {
+    if (entries[0].isIntersecting) {
+        loadNextPage();
+    }
+}
+
+async function loadNextPage() {
+    page++;
+
+    const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchTxt.value}&per_page=20&page=${page}&sort=relevance&format=json&nojsoncallback=1`);
+    const data = await response.json();
+
+    console.log(data);
+
+    data.photos.photo.forEach(photo => {
+        const serverId = photo.server;
+        const id = photo.id;
+        const secret = photo.secret;
+        const thumbnail = document.createElement("img");
+        thumbnail.src = `https://live.staticflickr.com/${serverId}/${id}_${secret}_${thumbnailSize}.jpg`
+        searchResult.appendChild(thumbnail)
+    });
+
+    //Makes the images clickable for a larger version
+    const searchResultImgs = document.querySelectorAll(".searchResult img");
+    for (let index = imgsPerPage * (page - 1); index < searchResultImgs.length; index++) {
+
+        searchResultImgs[index].addEventListener("click", function () {
+            lightbox.classList.add("active");
+            const largeImg = document.createElement("img");
+            largeImg.src = searchResultImgs[index].src.replace(`_${thumbnailSize}.jpg`, `_${largeImgSize}.jpg`);
+            lightbox.appendChild(largeImg);
+        });
+    }
+}
 
 searchTxt.addEventListener("keyup", function (event) {
     if (event.key === 'Enter') {
