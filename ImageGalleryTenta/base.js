95afa7706e6f12c1c0620f400637ca0ebb03d7a7
nejstore <emilnestor@hotmail.com>
2021-05-12 10:55:54 +0200

    Added try-catch and result number

--- ImageGalleryTenta/base.js
+++ ImageGalleryTenta/base.js
@@ -3,6 +3,7 @@ const searchBar = document.querySelector(".searchBar");
 const searchTxt = document.querySelector(".searchTxt");
 const searchBtn = document.querySelector(".searchBtn");
 const searchResult = document.querySelector(".searchResult");
+const resultNumber = document.querySelector(".resultNumber");
 const scrollInstruction = document.querySelector(".scrollInstruction");
 const footer = document.querySelector("footer");
 const lightbox = document.querySelector(".lightbox");
@@ -22,40 +23,55 @@ const observer = new IntersectionObserver(handleIntersect, options);
 //https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
 
 searchBtn.addEventListener("click", async function () {
-    const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchTxt.value}&per_page=20&sort=relevance&format=json&nojsoncallback=1`);
-    const data = await response.json();
+    try {
+        const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchTxt.value}&per_page=20&sort=relevance&format=json&nojsoncallback=1`);
+        const data = await response.json();
 
-    //console.log(data);
+        if (data.stat == "fail") {            
+            throw data.message;
+        }
 
-    searchResult.innerHTML = "";
-    page = 1;
+        searchResult.innerHTML = "";
+        resultNumber.style.display = "none";
+        footer.style.display = "none";
+        page = 1;
 
-    data.photos.photo.forEach(photo => {
-        const serverId = photo.server;
-        const id = photo.id;
-        const secret = photo.secret;
-        const thumbnail = document.createElement("img");
-        thumbnail.src = `https://live.staticflickr.com/${serverId}/${id}_${secret}_${thumbnailSize}.jpg`
-        searchResult.appendChild(thumbnail)
-    });
+        console.log(data);
 
-    //Makes the images clickable for a larger version
-    const searchResultImgs = document.querySelectorAll(".searchResult img");
-    for (let index = imgsPerPage * (page - 1); index < searchResultImgs.length; index++) {
-
-        searchResultImgs[index].addEventListener("click", function () {
-            lightbox.classList.add("active");
-            const largeImg = document.createElement("img");
-            largeImg.src = searchResultImgs[index].src.replace(`_${thumbnailSize}.jpg`, `_${largeImgSize}.jpg`);
-            lightbox.appendChild(largeImg);
+        data.photos.photo.forEach(photo => {
+            const serverId = photo.server;
+            const id = photo.id;
+            const secret = photo.secret;
+            const thumbnail = document.createElement("img");
+            thumbnail.src = `https://live.staticflickr.com/${serverId}/${id}_${secret}_${thumbnailSize}.jpg`
+            searchResult.appendChild(thumbnail)
         });
+
+        //Makes the images clickable for a larger version
+        const searchResultImgs = document.querySelectorAll(".searchResult img");
+        for (let index = imgsPerPage * (page - 1); index < searchResultImgs.length; index++) {
+
+            searchResultImgs[index].addEventListener("click", function () {
+                lightbox.classList.add("active");
+                const largeImg = document.createElement("img");
+                largeImg.src = searchResultImgs[index].src.replace(`_${thumbnailSize}.jpg`, `_${largeImgSize}.jpg`);
+                lightbox.appendChild(largeImg);
+            });
+        }
+
+        resultNumber.style.display = "flex";
+        resultNumber.innerHTML = `"${searchTxt.value}" gave ${data.photos.total} results`;
+
+        if (data.photos.pages > 1) {
+            scrollInstruction.style.display = "flex";
+            footer.style.display = "flex";
+        }
+
+        observer.observe(footer);
+
+    } catch (error) {
+        alert(error);
     }
-
-    scrollInstruction.style.display = "flex";
-
-    footer.style.display = "flex";
-
-    observer.observe(footer);
 });
 
 function handleIntersect(entries) {
