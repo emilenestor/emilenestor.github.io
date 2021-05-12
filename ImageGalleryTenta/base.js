c7dd32af8d93aa9c7c18b52b54a549a784f50d9b
nejstore <emilnestor@hotmail.com>
2021-05-12 14:21:07 +0200

    Removed redundancy and added backToTop-button

--- ImageGalleryTenta/base.js
+++ ImageGalleryTenta/base.js
@@ -2,6 +2,7 @@ const apiKey = "ed033607eb107d8e7d8c5f174bd2209d";
 const searchBar = document.querySelector(".searchBar");
 const searchTxt = document.querySelector(".searchTxt");
 const searchBtn = document.querySelector(".searchBtn");
+const backToTop = document.querySelector(".backToTop");
 const searchResult = document.querySelector(".searchResult");
 const resultNumber = document.querySelector(".resultNumber");
 const scrollInstruction = document.querySelector(".scrollInstruction");
@@ -9,8 +10,9 @@ const footer = document.querySelector("footer");
 const lightbox = document.querySelector(".lightbox");
 const thumbnailSize = "q";
 const largeImgSize = "b";
-let page = 1;
 let imgsPerPage = 20;
+let page = 1;
+
 
 let options = {
     root: null,
@@ -23,20 +25,24 @@ const observer = new IntersectionObserver(handleIntersect, options);
 //https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
 
 searchBtn.addEventListener("click", async function () {
+    //Resets a few parameters
+    searchResult.innerHTML = "";
+    resultNumber.style.display = "none";
+    scrollInstruction.style.display = "none";
+    footer.style.display = "none";
+    page = 1;
+
+    await fetchData();
+});
+
+async function fetchData() {
     try {
-        const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchTxt.value}&per_page=20&sort=relevance&format=json&nojsoncallback=1`);
+        const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchTxt.value}&page=${page}&per_page=${imgsPerPage}&sort=relevance&format=json&nojsoncallback=1`);
         const data = await response.json();
 
-        if (data.stat == "fail") {            
+        if (data.stat == "fail") {
             throw data.message;
         }
-
-        //Resets a few parameters
-        searchResult.innerHTML = "";
-        resultNumber.style.display = "none";
-        scrollInstruction.style.display = "none";
-        footer.style.display = "none";
-        page = 1;
 
         console.log(data);
 
@@ -72,9 +78,9 @@ searchBtn.addEventListener("click", async function () {
         observer.observe(footer);
 
     } catch (error) {
-        alert(error);
+        searchResult.innerHTML = "Error: " + error;
     }
-});
+}
 
 function handleIntersect(entries) {
     if (entries[0].isIntersecting) {
@@ -83,33 +89,9 @@ function handleIntersect(entries) {
 }
 
 async function loadNextPage() {
+
     page++;
-
-    const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apiKey}&text=${searchTxt.value}&per_page=20&page=${page}&sort=relevance&format=json&nojsoncallback=1`);
-    const data = await response.json();
-
-    console.log(data);
-
-    data.photos.photo.forEach(photo => {
-        const serverId = photo.server;
-        const id = photo.id;
-        const secret = photo.secret;
-        const thumbnail = document.createElement("img");
-        thumbnail.src = `https://live.staticflickr.com/${serverId}/${id}_${secret}_${thumbnailSize}.jpg`
-        searchResult.appendChild(thumbnail)
-    });
-
-    //Makes the images clickable for a larger version
-    const searchResultImgs = document.querySelectorAll(".searchResult img");
-    for (let index = imgsPerPage * (page - 1); index < searchResultImgs.length; index++) {
-
-        searchResultImgs[index].addEventListener("click", function () {
-            lightbox.classList.add("active");
-            const largeImg = document.createElement("img");
-            largeImg.src = searchResultImgs[index].src.replace(`_${thumbnailSize}.jpg`, `_${largeImgSize}.jpg`);
-            lightbox.appendChild(largeImg);
-        });
-    }
+    await fetchData();
 }
 
 searchTxt.addEventListener("keyup", function (event) {
@@ -129,3 +111,17 @@ lightbox.addEventListener("click", function (event) {
     //Removes the appended image
     lightbox.innerHTML = "";
 });
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        backToTop.style.display = "flex";
+    } else {
+        backToTop.style.display = "none";
+    }
+};
+
+backToTop.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
